7e2a59f1e8117cfcd59ec768bc27f76bf24b6635
mohamad <mohamad97mj@gmail.com>
2021-02-18 13:44:07 +0330

    url for views commented

--- bio_check_api/urls.py
+++ bio_check_api/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main.views import WelcomeView
+# from main.views import WelcomeView
 from django.conf.urls.static import static
 from django.conf import settings
 
