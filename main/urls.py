60632e78067f2bd144ff593d24a1a5b93b59f45e
mohamad <mohamad97mj@gmail.com>
2021-02-18 13:41:22 +0330

    urls commented

--- main/urls.py
+++ main/urls.py
@@ -7,13 +7,13 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-                  path('predict', views.PredictionView.as_view(), name='predict'),
-                  path('bulk-predict', views.BulkPredictionView.as_view(), name='bulk_predict'),
-                  path('tag', views.TagView.as_view(), name='tag'),
-                  path('bulk-tag', views.BulkTagView.as_view(), name='bulk_tag'),
-                  path('test', views.TestView.as_view(), name='test'),
-                  path('train-model', views.TrainModelView.as_view(), name='train_model'),
-                  path('load-model', views.LoadModelView.as_view(), name='load_model'),
+                  # path('predict', views.PredictionView.as_view(), name='predict'),
+                  # path('bulk-predict', views.BulkPredictionView.as_view(), name='bulk_predict'),
+                  # path('tag', views.TagView.as_view(), name='tag'),
+                  # path('bulk-tag', views.BulkTagView.as_view(), name='bulk_tag'),
+                  # path('test', views.TestView.as_view(), name='test'),
+                  # path('train-model', views.TrainModelView.as_view(), name='train_model'),
+                  # path('load-model', views.LoadModelView.as_view(), name='load_model'),
               ]
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
