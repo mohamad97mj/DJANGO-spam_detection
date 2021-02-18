f1e7c816a77227ee8245604b1d3518788cc45112
mohamad <mohamad97mj@gmail.com>
2021-02-18 13:38:46 +0330

    urls commented

--- bio_check_api/urls.py
+++ bio_check_api/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('api/', include('main.urls')),
-                  path('', WelcomeView.as_view()),
+                  # path('api/', include('main.urls')),
+                  # path('', WelcomeView.as_view()),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
